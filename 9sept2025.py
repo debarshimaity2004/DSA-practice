f0d5891d2ec44a48c56f4d2f89a581e628cbc64f
debarshimaity2004 <debarshimaity14@gmail.com>
2025-09-09 18:21:26 +0530

    Create 9sept2025.py

---
+++ 9sept2025.py
@@ -0,0 +1,27 @@
+#47. Permutations II
+#timecomplexity...O(N∗N!)
+#spacecomplexity...O(N∗N!)
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        perm = []
+        count = {n: 0 for n in nums}
+        for num in nums:
+            count[num] += 1
+
+        def dfs():
+            if len(perm) == len(nums):
+                res.append(perm.copy())
+                return
+
+            for num in count:
+                if count[num] > 0:
+                    perm.append(num)
+                    count[num] -= 1
+                    dfs()
+                    count[num] += 1
+                    perm.pop()
+
+        dfs()
+        return res
