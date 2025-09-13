148e2d38fcc31b72c72d95e5313106731d9ebf3d
debarshimaity2004 <debarshimaity14@gmail.com>
2025-09-13 14:31:52 +0530

    Create 13sept2025.py

---
+++ python/sept2025/13sept2025.py
@@ -0,0 +1,31 @@
+#473. Matchsticks to Square
+#time O(4^n) space O(n)
+
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        total_length = sum(matchsticks)
+        if total_length % 4 != 0:
+            return False
+
+        length = total_length // 4
+        sides = [0] * 4
+        matchsticks.sort(reverse=True)
+
+        def dfs(i):
+            if i == len(matchsticks):
+                return True
+
+            for side in range(4):
+                if sides[side] + matchsticks[i] <= length:
+                    sides[side] += matchsticks[i]
+                    if dfs(i + 1):
+                        return True
+                    sides[side] -= matchsticks[i]
+
+                if sides[side] == 0:
+                    break
+
+            return False
+
+        return dfs(0)
+        
