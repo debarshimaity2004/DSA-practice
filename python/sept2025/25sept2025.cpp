3235662b9280812b35a47f554fea99e77506df16
debarshimaity2004 <debarshimaity14@gmail.com>
2025-09-25 15:36:46 +0530

    Create 25sept2025.cpp

---
+++ python/sept2025/25sept2025.cpp
@@ -0,0 +1,19 @@
+//338.Counting Bits
+//tc...O(n)
+//sc...O(1)
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> dp(n + 1);
+        int offset = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (offset * 2 == i) {
+                offset = i;
+            }
+            dp[i] = 1 + dp[i - offset];
+        }
+        return dp;
+    }
+};
