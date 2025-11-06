5c27772aa0c0ba65317cf93693801358956e4ee9
debarshimaity2004 <debarshimaity14@gmail.com>
2025-11-06 14:10:40 +0530

    Create 53_Maximum_Subarray.cpp

---
+++ nov2025/53_Maximum_Subarray.cpp
@@ -0,0 +1,22 @@
+//kadanes Algorithm
+//tc : O(n)
+//sc : O(1)
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        long long sum=0;
+        long long maxi=LONG_MIN;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            sum +=nums[i];
+            if(sum>maxi){
+                maxi=sum;
+            }
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return maxi;
+    }
+};
