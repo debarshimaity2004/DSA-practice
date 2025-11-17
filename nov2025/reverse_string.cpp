d197496accc2bc183551c6f249ee5efa083990e6
Debarshi Maity <debarshimaity14@gmail.com>
2025-11-17 20:11:34 +0530

    Add reverseString function to reverse a string
    Implement reverseString method to reverse a string in place.

---
+++ nov2025/reverse_string.cpp
@@ -0,0 +1,19 @@
+// TC:O(N)
+// SC:O(1)
+
+class Solution {
+  public:
+    string reverseString(string& s) {
+        // code here
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+        return s;
+    }
+};
