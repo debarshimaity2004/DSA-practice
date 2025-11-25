25c2c4182ae216b3fb9cb56582fd98dd0dc8ca78
Debarshi Maity <debarshimaity14@gmail.com>
2025-11-25 20:48:02 +0530

    Optimize findUnion method for union of arrays
    Refactored findUnion method to use sorting and two-pointer technique for space optimization.

--- nov2025/union_of_arrays_with_duplicates.cpp
+++ nov2025/union_of_arrays_with_duplicates.cpp
@@ -1,13 +1,65 @@
+// tc...o(n+m)
+// sc...o(n+m)
+
+// class Solution {
+//   public:
+//     vector<int> findUnion(vector<int>& a, vector<int>& b) {
+//         // code here
+//         unordered_set<int> s;
+
+//         for (int x : a) s.insert(x);
+//         for (int x : b) s.insert(x);
+
+//         vector<int> res(s.begin(), s.end());
+//         return res;
+//     }
+// };
+
+//method-2:space optimized
+// tc:O(nlogn +mlogm)
+// sc:O(1)
+
 class Solution {
-  public:
+public:
     vector<int> findUnion(vector<int>& a, vector<int>& b) {
-        // code here
-        unordered_set<int> s;
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
 
-        for (int x : a) s.insert(x);
-        for (int x : b) s.insert(x);
+        vector<int> res;
+        int i = 0, j = 0;
 
-        vector<int> res(s.begin(), s.end());
+        while (i < a.size() && j < b.size()) {
+            if (a[i] == b[j]) {
+                if (res.empty() || res.back() != a[i])
+                    res.push_back(a[i]);
+                i++; 
+                j++;
+            }
+            else if (a[i] < b[j]) {
+                if (res.empty() || res.back() != a[i])
+                    res.push_back(a[i]);
+                i++;
+            }
+            else {
+                if (res.empty() || res.back() != b[j])
+                    res.push_back(b[j]);
+                j++;
+            }
+        }
+
+        while (i < a.size()) {
+            if (res.empty() || res.back() != a[i])
+                res.push_back(a[i]);
+            i++;
+        }
+
+        while (j < b.size()) {
+            if (res.empty() || res.back() != b[j])
+                res.push_back(b[j]);
+            j++;
+        }
+
         return res;
     }
 };
+
