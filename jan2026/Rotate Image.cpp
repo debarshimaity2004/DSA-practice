216a6e8d30d853a7ffb0a3360e1919f37e1aa3f2
Debarshi Maity <debarshimaity14@gmail.com>
2026-01-14 20:09:59 +0530

    array medium section solving

---
+++ jan2026/Rotate Image.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<vector<int>> matrix(n, vector<int>(n));
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    // transpose
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = i + 1; j < n; j++) {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+
+    // reverse each row
+    for(int i = 0; i < n; i++) {
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+
+    // print result
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
