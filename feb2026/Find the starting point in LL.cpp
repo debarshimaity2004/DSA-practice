929dd938d09fc0cf812abcd26bc261d65d151a45
Debarshi Maity <debarshimaity14@gmail.com>
2026-02-12 19:42:01 +0530

    singly linked list solving

---
+++ feb2026/Find the starting point in LL.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode{
+    int val;
+    ListNode* next;
+    ListNode(int x){
+        val = x;
+        next = NULL;
+    }
+};
+
+ListNode* detectCycle(ListNode* head){
+
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while(fast != NULL && fast->next != NULL){
+
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if(slow == fast){
+
+            slow = head;
+
+            while(slow != fast){
+                slow = slow->next;
+                fast = fast->next;
+            }
+
+            return slow;
+        }
+    }
+
+    return NULL;
+}
+
+int main(){
+
+    ListNode* head = new ListNode(3);
+    ListNode* n1 = new ListNode(2);
+    ListNode* n2 = new ListNode(0);
+    ListNode* n3 = new ListNode(-4);
+
+    head->next = n1;
+    n1->next = n2;
+    n2->next = n3;
+    n3->next = n1; // cycle
+
+    ListNode* ans = detectCycle(head);
+
+    if(ans != NULL)
+        cout << ans->val << '\n';
+    else
+        cout << "No cycle\n";
+
+    return 0;
+}
